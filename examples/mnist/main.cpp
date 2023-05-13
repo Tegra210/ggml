e2a1d15744c54ccb6933db39fed0fdaa955e01e1
Bhood23 <80179069+Bhood23@users.noreply.github.com>
2023-05-14 09:12:54 +1000

    Update main.cpp

--- examples/mnist/main.cpp
+++ examples/mnist/main.cpp
@@ -12,6 +12,27 @@
 #include <vector>
 #include <iostream>
 #include <unistd.h>
+
+#ifdef _WIN32
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+#include <windows.h>
+#include <io.h>
+
+#define STDIN_FILENO _fileno( stdin )
+#define STDOUT_FILENO _fileno( stdout )
+#define STDERR_FILENO _fileno( stdout )
+
+#define isatty _isatty
+
+#undef min
+#undef max
+
+#else
+#include <unistd.h>
+#endif
+
 #include <time.h>
 #include <algorithm>
 
